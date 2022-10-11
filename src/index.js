3c896e017e17015793a1c4e2645c7044ec4794d0
Saman Batool <samanbatool08@gmail.com>
2022-10-11 15:16:03 -0400

    showing comment details with faker

--- src/index.js
+++ src/index.js
@@ -1,23 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { faker } from '@faker-js/faker';
 
 const App = () => {
   return (
     <div className="ui container comments">
       <div className="comment">
         <a href="/" className="avatar">
-          <img alt="avatar" />
+          <img alt="avatar" src={faker.image.avatar()}/>
 
         </a>
         <div className="content">
           <a href="/" className="author">
             Sam
           </a>
-        </div>
         <div className="metadata">
           <span className="date">Today at 6:00 PM</span>
         </div>
         <div className="text">Nice blog post!</div>
+        </div>
       </div>
     </div>
   )
