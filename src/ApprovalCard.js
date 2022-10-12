fe867c23166dede32c4dd78a559f51c07533d962
Saman Batool <samanbatool08@gmail.com>
2022-10-12 11:50:44 -0400

    showing custom children

--- src/ApprovalCard.js
+++ src/ApprovalCard.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const ApprovalCard = () => {
+const ApprovalCard = (props) => {
   return (
     <div className='ui card'>
-      <div className='ui content'>Are you sure?</div>
+      <div className='ui content'>{props.children}</div>
       <div className='extra content'>
         <div className='ui two buttons'>
           <div className='ui basic green button'>Approve</div>
--- src/index.js
+++ src/index.js
@@ -7,10 +7,17 @@ import ApprovalCard from './ApprovalCard';
 const App = () => {
   return (
     <div className="ui container comments">
-      <ApprovalCard />
+      <ApprovalCard>
       <CommentDetail author="Sam" time="Today at 6:00PM" content="First comment" avatar={faker.image.avatar()}/>
-      <CommentDetail author="Alex" time="Today at 9:30PM" content="My first blog post" avatar={faker.image.avatar()}/>
-      <CommentDetail author="Jane" time="Today at 11:00PM" content="Waiting for a response" avatar={faker.image.avatar()}/>
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail author="Alex" time="Today at 9:30PM" content="My first blog post" avatar={faker.image.avatar()}/>
+      </ApprovalCard>
+
+      <ApprovalCard>
+       <CommentDetail author="Jane" time="Today at 11:00PM" content="Waiting for a response" avatar={faker.image.avatar()}/>
+      </ApprovalCard>
     </div>
   )
 };
